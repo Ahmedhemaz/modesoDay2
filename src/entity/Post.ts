15e2cb39b2697ff0573d59f9c9ae7d0e9cbf1ff7
deenaahmed <deenaahmedkamal8@gmail.com>
2019-09-16 09:40:10 +0200

    Add Properties to post and user model

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,4 +1,5 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import {Contains, IsInt, Length, IsEmail, IsFQDN, IsDate, Min, Max} from "class-validator";
 export enum PostStatus{
     PUBLIC = "public",
     PRIVATE = "private"
@@ -22,6 +23,12 @@ export class Post{
         default: PostStatus.PUBLIC
     })
     status: PostStatus;
+
+    @CreateDateColumn({type: "timestamp"})
+    createdAt: Date;
+
+    @UpdateDateColumn({type: "timestamp"})
+    updatedAt: Date;
 
 
 }
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
 
 @Entity()
 export class User {
@@ -15,6 +15,16 @@ export class User {
     @Column()
     age: number;
 
-    
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @CreateDateColumn({type: "timestamp"})
+    createdAt: Date;
+
+    @UpdateDateColumn({type: "timestamp"})
+    updatedAt: Date;
 
 }
